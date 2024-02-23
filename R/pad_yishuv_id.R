097c01f51266a619e2dd2875094ab60ca5f60220
Matan <matanhakim@gmail.com>
2024-02-23 11:52:37 +0200

    Create initial documentation for `pad_yishuv_id()`

--- R/pad_yishuv_id.R
+++ R/pad_yishuv_id.R
@@ -1,3 +1,14 @@
+#' Pad a yishuv id
+#'
+#' @param yishuv_id
+#'
+#' @return A character vector, where each element is 4 characters long,
+#' containing only numbers and left-padded with 0's.
+#' @export
+#'
+#' @examples
+#' x <- c(1, "23", "4000", 5600)
+#' pad_yishuv_id(x)
 pad_yishuv_id <- function(yishuv_id) {
   stringr::str_pad(yishuv_id, width = 4, side = "left", pad = "0")
 }
