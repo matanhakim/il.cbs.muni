5bae62ae1c9e681ec89b1abb4aea6f72fd823bb9
Matan <matanhakim@gmail.com>
2024-02-23 15:29:51 +0200

    Add `clean_name()` function

---
+++ R/clean_name.R
@@ -0,0 +1,18 @@
+#' Clean a string from characters unwanted in (mostly) yishuv names
+#'
+#' @param name a character vector
+#'
+#' @return a character vector without leading and trailing white space, and with
+#' characters that are only letters, excluding the following: `'-()"`
+#' @export
+#'
+#' @examples
+#' clean_name("test-123_test*456")
+#' clean_name("אילון תבור*")
+#' clean_name("ג'סר א-זרקא")
+clean_name <- function(name) {
+  stopifnot(is.character(name))
+  name |>
+    stringr::str_remove_all("[[:punct:][:symbol:][:digit:]&&[^'\\-()\"]]") |>
+    stringr::str_squish()
+}
