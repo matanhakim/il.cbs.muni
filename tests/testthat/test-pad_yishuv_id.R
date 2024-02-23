68b5d4f12cf9134bb8e7687a4472a2f9fe56a0c1
Matan <matanhakim@gmail.com>
2024-02-23 12:45:43 +0200

    Add unit tests for input validation in `pad_yishuv_id()`

--- tests/testthat/test-pad_yishuv_id.R
+++ tests/testthat/test-pad_yishuv_id.R
@@ -4,3 +4,11 @@ test_that("pad_yishu_id() pads a character vector with a single element", {
   expect_equal(pad_yishuv_id("123"), "0123")
   expect_equal(pad_yishuv_id("1234"), "1234")
 })
+
+test_that("pad_yishuv_id() throws an error when input is too long", {
+  expect_error(pad_yishuv_id("12345"))
+})
+
+test_that("pad_yishuv_id() throws an error when input is  not character or numeric", {
+  expect_error(pad_yishuv_id(TRUE))
+})
