fee214d0ae029b88fe558b02e273af4ffe652a2d
Matan <matanhakim@gmail.com>
2024-02-25 15:14:40 +0200

    Rebuild `row_to_names_fill()` function to work without janitor

--- R/row_to_names_fill.R
+++ R/row_to_names_fill.R
@@ -1,33 +1,42 @@
-row_to_names_fill <- function(data, row_number, fill_missing = TRUE, remove_row = TRUE,
-                              remove_rows_above = TRUE, sep = "_") {
-  if (all(fill_missing)) {
-    fill_missing <- rep(TRUE, length(row_number))
+  row_to_names_fill <- function(data, row_number, fill_missing = TRUE, remove_row = TRUE,
+                          remove_rows_above = TRUE, sep = "_") {
+    col_names <- purrr::map2(
+      row_number,
+      fill_missing,
+      \(i, fill_missing) fill_row(data, row_number = i, fill_missing, sep = sep)
+    ) |>
+      dplyr::bind_cols() |>
+      suppressMessages() |>
+      tidyr::unite(col = "var_names", sep = sep, na.rm = TRUE) |>
+      dplyr::pull(var_names)
+
+    if (remove_row) {
+      data <- data |>
+        dplyr::slice(-row_number)
+    }
+
+    if (remove_rows_above & min(row_number) > 1) {
+      data <- data |>
+        dplyr::slice(-1:(-min(row_number) + 1))
+    }
+
+    names(data) <- col_names
+
+    data
   }
 
-  col_names <- data[row_number, ] |>
-    dplyr::mutate(
-      dplyr::across(dplyr::everything(), as.character)
-    ) |>
-    tidyr::pivot_longer(!1, names_to = ".names", values_to = ".value") |>
-    tidyr::pivot_wider(names_from = 1, values_from = ".value") |>
-    dplyr::select(!.names) |>
-    tidyr::fill(
-      which(fill_missing), .direction = "down"
-    ) |>
-    tidyr::unite(
-      "names",
-      dplyr::everything(),
-      remove = TRUE,
-      na.rm = TRUE
-    ) |>
-    dplyr::pull(names)
+  fill_row <- function(data, row_number, fill_missing, sep = "_") {
+    data <- data |>
+      dplyr::slice(row_number) |>
+      dplyr::mutate(dplyr::across(dplyr::everything(), as.character)) |>
+      tidyr::pivot_longer(dplyr::everything()) |>
+      dplyr::select(value)
 
-  data |>
-    janitor::row_to_names(
-      row_number = row_number,
-      remove_row = remove_row,
-      remove_rows_above = remove_rows_above
-    ) |>
-    setNames(col_names)
-}
+    if (fill_missing) {
+      data <- data |>
+        tidyr::fill(value)
+    }
 
+    data |>
+      dplyr::pull(value)
+  }
