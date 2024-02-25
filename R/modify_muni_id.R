fe2d0a5d2f4874d897637ba277ac1ab53deb335b
Matan <matanhakim@gmail.com>
2024-02-25 21:40:45 +0200

    Add a description paragraph for `modify_muni_id()`

--- R/modify_muni_id.R
+++ R/modify_muni_id.R
@@ -1,5 +1,16 @@
 #' Modify a municipal id depending on the municipal status and yishuv id
 #'
+#' In the context of working with cbs municipal and yishuvim data, there is a
+#' difference in the treatment of a yishuv and a municipality. In the cases of
+#' cities and local councils the yishuv id and the municipality id are the same.
+#' But in the case of regional councils, the municipality id is for the regional
+#' council, while every yishuv within that municipality has a different yishuv id.
+#' The Israeli CBS uses a concept of "municipal status" to differntiate between
+#' the two. A municipal status of "0" represents a city, and "99" represents a local
+#' council. Every other 2-digit number is the municipal id of a regional council.
+#' This function modifies a municipal status based on itself and the yishuv_id.
+#' It returns the correct municipal id after accounting for the "0" or "99" values.
+#'
 #' @param muni_id A character or numeric vector indicating the municipal status,
 #' where "0" or "99" represents a city or a local council, and every other two-digit
 #' number or character represents a regional council.
