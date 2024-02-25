3c506203a2f03116a7828a014ece31de1eed89d9
Matan <matanhakim@gmail.com>
2024-02-25 21:50:36 +0200

    Export `row_to_names_fill()`

--- R/row_to_names_fill.R
+++ R/row_to_names_fill.R
@@ -7,27 +7,23 @@
 #' the fill utility.
 #'
 #' @param data A data frame.
-#'
 #' @param row_number A numeric vector with he row indices of `data` containing the
 #' variable names. Allows for multiple rows input as a numeric vector. If multiple
 #' rows, values in the same column would be pasted with the `sep` argument as a
 #' separator. NA's are ignored.
-#'
 #' @param fill_missing A logical vector of length 1 or of length `length(row_number)`.
 #' Every value in the vector denotes for the matching row in `data[row_number, ]` if
 #' the row should fill missing values (from left to right). If `TRUE` for a row,
 #' all missing values following a non-missing value will be replaced with that
 #' preceding non-missing value.
-#'
 #' @param remove_row A logical vector of length 1, denoting if the row `row_number`
 #' should be removed from the resulting data.frame.
-#'
 #' @param remove_rows_above A logical vector of length 1, denoting if the rows above
 #' `row_number` - that is, between `1:(row_number-1)` - should be removed
 #' from the resulting data.frame, in the case that `row_number != 1`.
-#'
 #' @param sep A character vector of length 1 to separate the values in the case of
 #' multiple rows input to `row_number`.
+#' @export
 #'
 #' @examples
 #' df <- data.frame(
