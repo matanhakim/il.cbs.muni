1142fce1b0258e7d203e627e8918e5f13b952317
Matan <matanhakim@gmail.com>
2024-02-25 15:15:22 +0200

    Add tests, dependencies and example dataset for `row_to_names_fill()`

--- tests/testthat/test-row_to_names_fill.R
+++ tests/testthat/test-row_to_names_fill.R
@@ -5,4 +5,30 @@ test_that("fill and cast rows to names of a data frame", {
     c = letters[1:6]
   )
   expect_equal(row_to_names_fill(df_1, 1) |> names(), c("1", "x", "a"))
+  expect_equal(row_to_names_fill(df_1, 1:2) |> names(), c("1_2", "x_2", "a_b"))
+  expect_equal(row_to_names_fill(df_1, 2:4) |> names(), c("2_3_4", "2_x_4", "b_c_d"))
+})
+
+test_that("fill and cast rows to names of a data frame and keeps data well", {
+  df_1 <- data.frame(
+    a = 1:6,
+    b = rep(c("x", NA), 3),
+    c = letters[1:6]
+  )
+  expect_equal(row_to_names_fill(df_1, 1) |> unname(), dplyr::slice(df_1, 2:6) |> unname())
+  expect_equal(row_to_names_fill(df_1, 1:2, ) |> unname(), dplyr::slice(df_1, 3:6) |> unname())
+  expect_equal(row_to_names_fill(df_1, 2:5, ) |> unname(), dplyr::slice(df_1, 6) |> unname())
+})
+
+test_that("fill and cast rows to names of a data frame in a real data set", {
+  df_1 <- readxl::read_excel(here::here("inst/extdata/p_libud_2021.xlsx"), sheet = 2, col_types = "text", col_names = FALSE) |>
+    suppressMessages()
+  expect_equal(
+    row_to_names_fill(df_1, 4:5, fill_missing = c(TRUE, FALSE)) |> names() |> dplyr::nth(20),
+    "סה\"כ גברים בסוף השנה"
+  )
+  expect_equal(
+    row_to_names_fill(df_1, 4:5, fill_missing = c(TRUE, FALSE)) |> names() |> dplyr::nth(22),
+    "אחוז באוכלוסייה בסוף השנה_בני 4-0"
+  )
 })
