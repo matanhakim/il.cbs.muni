4c355bd19d4e7c456fba9a0dc3aaaf1c0b025413
Matan <matanhakim@gmail.com>
2024-02-26 13:38:27 +0200

    Fix filling problem in 2021-2 municipal budget data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -10,8 +10,8 @@ df_cbs_muni_params <- tibble::tibble(
     list(c(2, 4:5)), list(3:7), # 2018
     list(c(2, 4:5)), list(3:7), # 2019
     list(c(2, 4:5)), list(3:7), # 2020
-    list(c(2, 4:5)), list(3:4), # 2021
-    list(c(2, 4:5)), list(3:4) # 2022
+    list(c(2, 4:5)), list(4), # 2021
+    list(c(2, 4:5)), list(4) # 2022
   ),
   fill_missing = c(
     list(c(TRUE, TRUE, FALSE)), list(c(TRUE, TRUE, FALSE)), # 2016
@@ -19,8 +19,8 @@ df_cbs_muni_params <- tibble::tibble(
     list(c(TRUE, TRUE, FALSE)), list(c(TRUE, FALSE, FALSE, FALSE, FALSE)), # 2018
     list(c(TRUE, TRUE, FALSE)), list(c(TRUE, FALSE, FALSE, FALSE, FALSE)), # 2019
     list(c(TRUE, TRUE, FALSE)), list(c(TRUE, FALSE, FALSE, FALSE, FALSE)), # 2020
-    list(c(TRUE, TRUE, FALSE)), list(c(TRUE, FALSE)), # 2021
-    list(c(TRUE, TRUE, FALSE)), list(c(TRUE, FALSE)) # 2022
+    list(c(TRUE, TRUE, FALSE)), list(c(TRUE)), # 2021
+    list(c(TRUE, TRUE, FALSE)), list(c(TRUE)) # 2022
   )
 )
 
